9e2a41280e5f8864539ec3d4bbbf2870e6892d82
IBEPP <ibrahimajahitee@gmail.com>
2022-04-17 19:42:19 +0200

    react-js

---
+++ porsuite/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const caseStyle = {
+  width: "30px",
+  height: "30px",
+  border: "1px solid black",
+};
+
+const styleColored = {
+  width: "30px",
+};
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
